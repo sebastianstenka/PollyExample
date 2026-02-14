a4a6f61d3134495fa1aae4e3c8f57975620189f1
sebastian_stenka <sebastian.stenka@solwena.com>
2026-02-14 14:24:50 +0100

    Add LinearHttpRetry

--- RequestService/Policies/ClientPolicy.cs
+++ RequestService/Policies/ClientPolicy.cs
@@ -6,10 +6,14 @@ using Polly.Retry;
 public class ClientPolicy
 {
 	public AsyncRetryPolicy<HttpResponseMessage> ImmediateHttpRetry { get; set; }
+	public AsyncRetryPolicy<HttpResponseMessage> LinearHttpRetry { get; set; }
 
 	public ClientPolicy()
 	{
 		ImmediateHttpRetry = Policy.HandleResult<HttpResponseMessage>(
 				res => !res.IsSuccessStatusCode).RetryAsync(5);
+
+		LinearHttpRetry = Policy.HandleResult<HttpResponseMessage>(
+			res => !res.IsSuccessStatusCode).WaitAndRetryAsync(5, _ => TimeSpan.FromSeconds(3));
 	}
 }
