01bfe4e514c22a6d261b2b556a6bf0877a6eff3b
sebastian_stenka <sebastian.stenka@solwena.com>
2026-02-14 14:43:20 +0100

    Create http client with policy

--- RequestService/Controllers/RequestController.cs
+++ RequestService/Controllers/RequestController.cs
@@ -2,18 +2,14 @@ namespace RequestService.Controllers;
 
 using Microsoft.AspNetCore.Mvc;
 
-using Policies;
-
 [Route("api/[Controller]")]
 [ApiController]
-public class RequestController(ClientPolicy clientPolicy, IHttpClientFactory clientFactory) : ControllerBase
+public class RequestController(IHttpClientFactory clientFactory) : ControllerBase
 {
 	[HttpGet]
 	public async Task<ActionResult> MakeRequest()
 	{
-		var client = clientFactory.CreateClient();
-
-		var response = await clientPolicy.ExponentialHttpRetry.ExecuteAsync(() => client.GetAsync("http://localhost:5020/api/response/25"));
+		using var response = await clientFactory.CreateClient("ClientWithImmediateHttpRequest").GetAsync("http://localhost:5020/api/response/25");
 
 		if (response.IsSuccessStatusCode)
 		{
